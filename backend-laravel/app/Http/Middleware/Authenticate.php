5b6378269977131548894fde787a188a671cf607
nhiendev <nhiennkrb@gmail.com>
2025-09-27 16:28:00 +0700

    edit: change redirectTo when route not found

--- backend-laravel/app/Http/Middleware/Authenticate.php
+++ backend-laravel/app/Http/Middleware/Authenticate.php
@@ -10,8 +10,18 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    // protected function redirectTo(Request $request): ?string
+    // {
+    //     return $request->expectsJson() ? null : route('login');
+    // }
+
+     protected function redirectTo($request)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (! $request->expectsJson()) {
+            abort(response()->json([
+                'status' => false,
+                'message' => 'Unauthenticated.'
+            ], 401));
+        }
     }
 }
